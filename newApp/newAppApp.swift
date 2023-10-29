a527e09fcce8cbaf4b5f6310dcf648e9de8310d1
bryangarcia <bryan.checkoapp@gmail.com>
2023-10-29 13:28:38 -0600

    Initial Commit

---
+++ newApp/newAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  newAppApp.swift
+//  newApp
+//
+//  Created by bryangarcia on 29/10/23.
+//
+
+import SwiftUI
+
+@main
+struct newAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
